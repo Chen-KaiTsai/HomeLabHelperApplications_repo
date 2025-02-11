b0d8a88c4fcc581374b5bcc5709ab5b7f89716e0
ErebusTsai <92940171+Chen-KaiTsai@users.noreply.github.com>
2025-02-11 21:33:08 +0800

    Add files via upload

--- EPYC_Server_Bash/powersave_setting.sh
+++ EPYC_Server_Bash/powersave_setting.sh
@@ -1,3 +1,14 @@
+printf "Start Applying Power Saving Settings...\n\n"
+
+printf "\n\nStart powertop\n\n"
 sudo powertop --auto-tune
+
+printf "\n\nStart cpupower\n\n"
 sudo cpupower -c all frequency-set -g powersave
+
+printf "\n\nPrevent CPU from overclocking (CPU will run at base freq)\n"
+
+printf "Frequency boost:"
 echo 0 | sudo tee /sys/devices/system/cpu/cpufreq/boost
+
+printf "\n\nFinish\n\n"
